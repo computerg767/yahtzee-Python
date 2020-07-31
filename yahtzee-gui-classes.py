cc4ec890ef9c5de69ec96934acadedf3cf1c5aaf
Jacob Bibby <computerg767@gmail.com>
2020-07-31 10:54:40 -0400

    Functools added, button changes colour accordingly
    functiontools added an new way to reduce lines of code.
    took out a for loop that wasn't needed and combined it into one.
    added diceclicked array to calculate if the dice needs to be rolled or not.
    added def click to make it either red or black depending if a roll needs to be calculated

--- yahtzee-gui-classes.py
+++ yahtzee-gui-classes.py
@@ -1,7 +1,11 @@
 from tkinter import *
 import random
+from functools import partial
 
 root = Tk()
+
+root.title("Yahtzee")
+
 # Variables Start
 
 roll = 0
@@ -10,12 +14,21 @@ dicenames = ["one", "two", "three", "four", "five"]
 
 dicenumbers = [0,0,0,0,0]
 
+diceclicked = [1,1,1,1,1]
+
 score = 0
 
-for i in range(5):
-    dicenames[i] = Button(root, text=dicenumbers[i], padx=15, pady=15)
+def click(n):
+    diceclicked[n] = diceclicked[n] + 1
+    if (diceclicked[n] % 2) == 0:
+        dicenames[n].configure(background="red", activebackground="red")
+    else:
+        dicenames[n].configure(background="black", activebackground="black")
 
-for d in range(5):
-    dicenames[d].grid(row=10, column=d)
+
+
+for i in range(5):
+    dicenames[i] = Button(root, state="normal", relief="raised", text=dicenumbers[i], padx=15, pady=15, command=partial(click, i))
+    dicenames[i].grid(row=10, column=i)
 
 root.mainloop()
